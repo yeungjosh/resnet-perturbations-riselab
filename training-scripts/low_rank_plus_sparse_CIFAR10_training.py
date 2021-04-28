bc33b1e9b985e0b7f6008b55a220e53f3a8c9c34
Geoffrey Négiar <geoffrey_negiar@berkeley.edu>
2021-04-28 15:31:53 -0700

    Fixed batch size argparse arg

--- training-scripts/low_rank_plus_sparse_CIFAR10_training.py
+++ training-scripts/low_rank_plus_sparse_CIFAR10_training.py
@@ -120,9 +120,9 @@ def main():
 
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch_size', type=int, default=128, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+    parser.add_argument('--test_batch_size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=5, metavar='N',
                         help='number of epochs to train (default: 5)')
