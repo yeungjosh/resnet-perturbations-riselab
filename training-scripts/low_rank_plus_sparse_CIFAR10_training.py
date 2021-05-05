255006c30ca15257e74712e9705ef122868d9f5e
Geoffrey Négiar <geoffrey_negiar@berkeley.edu>
2021-05-05 15:22:06 -0700

    Using Ben's ResNet model + added possibility of training w/ SGD baseline

--- training-scripts/low_rank_plus_sparse_CIFAR10_training.py
+++ training-scripts/low_rank_plus_sparse_CIFAR10_training.py
@@ -17,7 +17,7 @@ from tqdm import tqdm
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torchvision.models import resnet18, resnet50
+from resnet import ResNet
 
 import chop
 
@@ -42,7 +42,7 @@ wandb.init(project='low-rank_sparse_cifar10', config=default_config)
 config = wandb.config
 
 
-def get_sparsity_and_rank(opt):
+def get_sparsity_and_rank(opt, splitting=True):
     nnzero = 0
     n_params = 0
     total_rank = 0
@@ -50,23 +50,32 @@ def get_sparsity_and_rank(opt):
 
     for group in opt.param_groups:
         for p in group['params']:
-            state = opt.state[p]
-            nnzero += (state['x'] != 0).sum()
+            if splitting:
+                state = opt.state[p]
+                nnzero += (state['x'] != 0).sum()
+                ranks = torch.linalg.matrix_rank(state['y'])
+
+            else:
+                nnzero += (p != 0).sum()
+                if p.ndim > 1:
+                    ranks = torch.linalg.matrix_rank(p)
+
             n_params += p.numel()
-            ranks = torch.linalg.matrix_rank(state['y'])
-            total_rank += ranks.sum()
-            max_rank += min(p.shape) * ranks.numel()
+            if p.ndim > 1:
+                total_rank += ranks.sum()
+                max_rank += min(p.shape) * ranks.numel()
 
     return nnzero / n_params, total_rank / max_rank
 
 
-def train(args, model, device, train_loader, opt, opt_bias, epoch, train_loss):
+def train(args, model, device, train_loader, opt, opt_bias, epoch, train_loss, splitting=True):
     model.train()
     for batch_idx, (data, target) in tqdm(enumerate(train_loader),
                                           desc=f'Training epoch {epoch}'):
         data, target = data.to(device), target.to(device)
         opt.zero_grad()
-        opt_bias.zero_grad()
+        if splitting:
+            opt_bias.zero_grad()
         output = model(data)
         loss = F.cross_entropy(output, target)
         loss.backward()
@@ -74,7 +83,8 @@ def train(args, model, device, train_loader, opt, opt_bias, epoch, train_loss):
             warnings.warn("Train loss is nan.")
             break
         opt.step()
-        opt_bias.step()
+        if splitting:
+            opt_bias.step()
         train_loss(loss.item(), len(target))
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
@@ -85,7 +95,7 @@ def train(args, model, device, train_loader, opt, opt_bias, epoch, train_loss):
     return loss
 
 
-def test(args, model, device, test_loader, opt, epoch):
+def test(args, model, device, test_loader, opt, epoch, splitting=True):
     model.eval()
     test_loss = 0
     correct = 0
@@ -108,7 +118,7 @@ def test(args, model, device, test_loader, opt, epoch):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-    sparsity, rank = get_sparsity_and_rank(opt)
+    sparsity, rank = get_sparsity_and_rank(opt, splitting)
     wandb.log({
         # "Examples": example_images,
         "Test Accuracy": 100. * correct / len(test_loader.dataset),
@@ -222,14 +232,14 @@ def main():
 
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Example')
-    parser.add_argument('--resnet_width', type=int, default=18)
+    parser.add_argument('--resnet_depth', type=int, default=20)
     parser.add_argument('--batch_size', type=int, default=128, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test_batch_size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=5, metavar='N',
                         help='number of epochs to train (default: 5)')
-    parser.add_argument('--lr',  default=1e-2, metavar='LR',
+    parser.add_argument('--lr',  default=1e-1, metavar='LR',
                         help='learning rate (default: "sublinear")')
     parser.add_argument('--lipschitz', default=1., type=float, metavar='LIP',
                         help='Lipschitz estimate, used in the prox (default: 1.)')
@@ -239,7 +249,7 @@ def main():
     parser.add_argument('--lr_decay_step', default=25, type=int)
     parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                         help='Optimizer momentum (default: 0.9)')
-    parser.add_argument('--weight_decay', type=float, default=1e-4, metavar='W',
+    parser.add_argument('--weight_decay', type=float, default=5e-4, metavar='W',
                         help='Optimizer weight decay (default: 0.)')
     parser.add_argument('--grad_norm', type=str, default='gradient',
                         help='Gradient normalization options')
@@ -247,7 +257,7 @@ def main():
                         help='Size of the Nuclear norm Ball constraint')
     parser.add_argument('--l1_constraint_size', type=float, default=1e3,
                         help='Size of the ell-1 norm Ball constraint')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
+    parser.add_argument('--no_cuda', action='store_true', default=False,
                         help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
@@ -255,6 +265,7 @@ def main():
                         help='how many batches to wait before logging training status')
     parser.add_argument('--retraction', type=bool, default=True,
                         help='enable retraction of the learning rate')
+    parser.add_argument('--no_splitting', action='store_true', default=False)
 
     # You can also enable retraction of the learning rate, i.e.,
     # if enabled the learning rate
@@ -279,84 +290,88 @@ def main():
         args.batch_size, args.test_batch_size, num_workers=0)
 
     print("Preparing model...")
-    if args.resnet_width == 18:
-        model = resnet18(num_classes=10).to(device)
-    elif args.resnet_width == 50:
-        model = resnet50(num_classes=10).to(device)
-    else:
-        raise ValueError("Can only use resnet18 or resnet50 currently.")
-    print("Make constraints...")
-    constraints_sparsity = chop.constraints.make_model_constraints(model,
-                                                                   ord=1,
-                                                                   value=args.l1_constraint_size,
-                                                                   constrain_bias=False)
-    constraints_low_rank = chop.constraints.make_model_constraints(model,
-                                                                   ord='nuc',
-                                                                   value=args.nuc_constraint_size,
-                                                                   constrain_bias=False)
-    proxes = [constraint.prox if constraint else None
-              for constraint in constraints_sparsity]
-    lmos = [constraint.lmo if constraint else None
-            for constraint in constraints_low_rank]
+    model = ResNet(depth=args.resnet_depth, num_classes=10).to(device)
 
-    proxes_lr = [constraint.prox if constraint else None
-                 for constraint in constraints_low_rank]
+    if args.no_splitting:
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_decay_step, gamma=args.lr_decay)
+        bias_opt = None
+    else:      
+        print("Make constraints...")
+        constraints_sparsity = chop.constraints.make_model_constraints(model,
+                                                                    ord=1,
+                                                                    value=args.l1_constraint_size,
+                                                                    constrain_bias=False)
+        constraints_low_rank = chop.constraints.make_model_constraints(model,
+                                                                    ord='nuc',
+                                                                    value=args.nuc_constraint_size,
+                                                                    constrain_bias=False)
+        proxes = [constraint.prox if constraint else None
+                for constraint in constraints_sparsity]
+        lmos = [constraint.lmo if constraint else None
+                for constraint in constraints_low_rank]
 
-    # Unconstrain downsampling layers
-    for k, (name, param) in enumerate(model.named_parameters()):
-        if 'downsample' in name:
-            try:
-                *_, m, n = param.shape
-            except ValueError:
-                continue
-            if m == n == 1:
-                proxes[k], lmos[k], proxes_lr[k] = None, None, None
+        proxes_lr = [constraint.prox if constraint else None
+                    for constraint in constraints_low_rank]
 
-    # initialize some necessary metrics objects
-    train_loss, train_accuracy = AverageMeter(), AverageMeter()
-    test_loss, test_accuracy = AverageMeter(), AverageMeter()
+        # Unconstrain downsampling layers
+        for k, (name, param) in enumerate(model.named_parameters()):
+            if 'downsample' in name:
+                try:
+                    *_, m, n = param.shape
+                except ValueError:
+                    continue
+                if m == n == 1:
+                    proxes[k], lmos[k], proxes_lr[k] = None, None, None
 
-    # function to reset metrics
-    def reset_metrics():
-        train_loss.reset()
-        train_accuracy.reset()
+        # function to reset metrics
+        def reset_metrics():
+            train_loss.reset()
+            train_accuracy.reset()
 
-        test_loss.reset()
-        test_accuracy.reset()
+            test_loss.reset()
+            test_accuracy.reset()
 
-    print("Initialize optimizer...")
-    optimizer = chop.stochastic.SplittingProxFW(model.parameters(), lmos,
-                                                proxes,
-                                                lr=args.lr,
-                                                lipschitz=args.lipschitz,
-                                                momentum=args.momentum,
-                                                weight_decay=args.weight_decay,
-                                                normalization=args.grad_norm)
+        print("Initialize optimizer...")
+        optimizer = chop.stochastic.SplittingProxFW(model.parameters(), lmos,
+                                                    proxes,
+                                                    lr=args.lr,
+                                                    lipschitz=args.lipschitz,
+                                                    momentum=args.momentum,
+                                                    weight_decay=args.weight_decay,
+                                                    normalization=args.grad_norm)
 
-    scheduler = torch.optim.lr_scheduler.StepLR(
-        optimizer, step_size=args.lr_decay_step, gamma=args.lr_decay)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, step_size=args.lr_decay_step, gamma=args.lr_decay)
 
-    if args.retraction:
-        retractionScheduler = RetractionLR(optimizer=optimizer)
+        if args.retraction:
+            retractionScheduler = RetractionLR(optimizer=optimizer)
 
-    bias_params = (param for param, lmo in zip(model.parameters(), lmos)
-                   if lmo is not None)
-    bias_opt = torch.optim.SGD(bias_params, lr=args.lr_bias, momentum=.9)
-    bias_scheduler = torch.optim.lr_scheduler.StepLR(
-        bias_opt, step_size=args.lr_decay_step, gamma=args.lr_decay)
+        bias_params = (param for param, lmo in zip(model.parameters(), lmos)
+                    if lmo is not None)
+        bias_opt = torch.optim.SGD(bias_params, lr=args.lr_bias, momentum=args.momentum)
+        bias_scheduler = torch.optim.lr_scheduler.StepLR(
+            bias_opt, step_size=args.lr_decay_step, gamma=args.lr_decay)
+
 
     # wandb.watch(model, log_freq=1, log='all')
 
     print("Training...")
+
+    # initialize some necessary metrics objects
+    train_loss, train_accuracy = AverageMeter(), AverageMeter()
+    test_loss, test_accuracy = AverageMeter(), AverageMeter()
+    
     for epoch in range(1, args.epochs + 1):
-        loss = train(args, model, device, loaders.train, optimizer, bias_opt, epoch, train_loss)
+        loss = train(args, model, device, loaders.train, optimizer, bias_opt, epoch, train_loss, not args.no_splitting)
         if loss.isnan():
             break
-        test(args, model, device, loaders.test, optimizer, epoch)
+        test(args, model, device, loaders.test, optimizer, epoch, not args.no_splitting)
         scheduler.step()
-        bias_scheduler.step()
+        if not args.no_splitting:
+            bias_scheduler.step()
 
-        if args.retraction:
+        if args.retraction and not args.no_splitting:
             # Learning rate retraction
             retractionScheduler.update_averages(train_loss.result())
             retractionScheduler.step()
